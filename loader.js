d4ffd5182b56ea60cf210e0dd9a1eee5745691a3
Akshathadas <akshathadasmeetinay.22it@licet.ac.in>
2021-06-02 17:05:52 +0530

    lol

---
+++ loader.js
@@ -0,0 +1,24 @@
+$(window).on("load", function () {
+    document.getElementById("main").style.visibility = "hidden";
+    /* Progress Bar animation */
+    document.getElementById("loader").style.visibility = "visible";
+    setTimeout(function () {
+        document.getElementById("loader").style.visibility = "hidden";
+        document.getElementById("main").style.visibility = "visible";
+      }, 5000);
+     });
+
+    //  $(document).ready(function() {
+    //     //Preloader
+    //     preloaderFadeOutTime = 5000;
+    //     function hidePreloader() {
+    //     var preloader = $('#loader');
+    //     preloader.fadeOut(preloaderFadeOutTime);
+    //     }
+    //     hidePreloader();
+    //     });
+
+//   $(document).on("ready", function () {
+  
+//   });
+  
