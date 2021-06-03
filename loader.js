514f8f33ea6605c4c7aa1e2776de78a73a2f63e9
alisonroy <alisonroy12@gmail.com>
2021-06-03 11:48:46 +0530

    Logo Fix

--- loader.js
+++ loader.js
@@ -1,24 +1,23 @@
 $(window).on("load", function () {
-    document.getElementById("main").style.visibility = "hidden";
-    /* Progress Bar animation */
-    document.getElementById("loader").style.visibility = "visible";
-    setTimeout(function () {
-        document.getElementById("loader").style.visibility = "hidden";
-        document.getElementById("main").style.visibility = "visible";
-      }, 5000);
-     });
+  document.getElementById("main").style.visibility = "hidden";
+  /* Progress Bar animation */
+  document.getElementById("loader").style.visibility = "visible";
+  setTimeout(function () {
+    document.getElementById("loader").style.visibility = "hidden";
+    document.getElementById("main").style.visibility = "visible";
+  }, 5000);
+});
 
-    //  $(document).ready(function() {
-    //     //Preloader
-    //     preloaderFadeOutTime = 5000;
-    //     function hidePreloader() {
-    //     var preloader = $('#loader');
-    //     preloader.fadeOut(preloaderFadeOutTime);
-    //     }
-    //     hidePreloader();
-    //     });
+//  $(document).ready(function() {
+//     //Preloader
+//     preloaderFadeOutTime = 5000;
+//     function hidePreloader() {
+//     var preloader = $('#loader');
+//     preloader.fadeOut(preloaderFadeOutTime);
+//     }
+//     hidePreloader();
+//     });
 
 //   $(document).on("ready", function () {
-  
+
 //   });
-  
